d1426be0dfd0014aa35a8331bf9a2027f1dbc79c
Dmitry <dmitry@class2011.ru>
2016-05-21 03:05:16 +0300

    добавление открытия активити

--- uchange/app/src/main/java/com/example/dima/uchange/LoginActivity.java
+++ uchange/app/src/main/java/com/example/dima/uchange/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.dima.uchange;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -43,7 +44,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-
+    static final private int CHOOSE_ACTIVITY = 0;
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
@@ -188,7 +189,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            nextActivity();
         }
+    }
+
+    private void nextActivity(){
+        Intent intent=new Intent("ru.startandroid.intent.action.sendActivity");
+
+        //intent.putExtra("sendRequest",house.rooms.get(position)); // передача данных
+
+        startActivityForResult(intent, CHOOSE_ACTIVITY);
     }
 
     private boolean isEmailValid(String email) {
